5dac81767bacfafebaf96355bd19f4446415721e
TeddyHuZz <feigaming04XP@gmail.com>
2025-04-08 23:30:57 +0800

    Creating About, Experiences, and Projects files

---
+++ src/About.js
@@ -0,0 +1,11 @@
+// filepath: /home/wenfei/Documents/my-portfolio/src/About.js
+function About() {
+    return (
+      <div>
+        <h1>About Me</h1>
+        <p>This is the About page.</p>
+      </div>
+    );
+  }
+  
+  export default About;
