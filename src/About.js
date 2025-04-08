93f3ee0fe55e8b814f8fdb3bfb1568ed2c3a2777
TeddyHuZz <feigaming04XP@gmail.com>
2025-04-09 01:50:17 +0800

    Social Media

--- src/About.js
+++ src/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './About.css';
 import profilePicture from './assets/profile.jpg';
+import twitterLogo from './assets/twitter.png'; // Replace with your logo paths
+import linkedinLogo from './assets/linkedin.png';
+import githubLogo from './assets/github.png';
 
 function About() {
   return (
@@ -18,6 +21,17 @@ function About() {
           Hey, I am Wen Fei! Software Developer and Web3{' '}
           <span className="typing-effect">Enthusiast</span>
         </h1>
+      </div>
+      <div className="social-media">
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <img src={twitterLogo} alt="Twitter" className="social-logo" />
+        </a>
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+          <img src={linkedinLogo} alt="LinkedIn" className="social-logo" />
+        </a>
+        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+          <img src={githubLogo} alt="GitHub" className="social-logo" />
+        </a>
       </div>
     </div>
   );
