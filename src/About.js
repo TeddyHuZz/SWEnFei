b99b9159951938da9fe6eb89f2eb35eeec155e51
TeddyHuZz <feigaming04XP@gmail.com>
2025-04-09 01:08:00 +0800

    Adding picture

--- src/About.js
+++ src/About.js
@@ -1,15 +1,22 @@
-// filepath: /home/wenfei/Documents/my-portfolio/src/About.js
 import React from 'react';
 import './About.css';
+import profilePicture from './home/wenfei/my-portfolio/src/assets/profile.jpg';
 
 function About() {
   return (
     <div className="container">
-      <nav className="navmenu">
-        <a href="#about">About</a>
-        <a href="#experience">Experience</a>
-        <a href="#projects">Projects</a>
-      </nav>
+      <div className="header">
+        <nav className="navmenu">
+          <a href="#about">About</a>
+          <a href="#experience">Experience</a>
+          <a href="#projects">Projects</a>
+        </nav>
+      </div>
+      <div className="content">
+        <img src={profilePicture} alt="Me" className="profile-picture" />
+        <h1>About Me</h1>
+        <p>This is the About page content.</p>
+      </div>
     </div>
   );
 }
