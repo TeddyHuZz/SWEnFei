3e1350903f0d2716db40916f9a1a6512ef808f1e
TeddyHuZz <feigaming04XP@gmail.com>
2025-04-09 22:59:54 +0800

    Adding projects/js

---
+++ src/Projects.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Projects.css';
+
+function Projects() {
+    return (
+      <div className="container">
+        <div className="header">
+          <nav className="navmenu">
+            <a href="#about">About</a>
+            <a href="#experience">Experience</a>
+            <a href="#projects">Projects</a>
+          </nav>
+        </div>
+        
+        
+        <div className="footer">
+          <p>© 2025 Wen Fei. All rights reserved.</p>
+          <p>Designed and Developed by Wen Fei</p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Projects;
