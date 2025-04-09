d3e89a17fa9d3625acff40fe7c56299e0a961767
TeddyHuZz <feigaming04XP@gmail.com>
2025-04-09 22:49:25 +0800

    Adding src/Experience.js

---
+++ src/Experience.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Experience.css';
+
+function Experience() {
+    return (
+      <div className="container">
+        <div className="header">
+          <nav className="navmenu">
+            <a href="#about">About</a>
+            <a href="#experience">Experience</a>
+            <a href="#projects">Projects</a>
+          </nav>
+        </div>
+        
+        
+        <div className="footer">
+          <p>© 2025 Wen Fei. All rights reserved.</p>
+          <p>Designed and Developed by Wen Fei</p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Experience;
