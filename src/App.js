5700d7bd7a87ae5f7af67466ef27808d2527d2f0
TeddyHuZz <feigaming04XP@gmail.com>
2025-04-09 22:58:27 +0800

    try

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './About';
+import Experience from './Experience';
+import Projects from './Projects';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
