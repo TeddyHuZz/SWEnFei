171ddd76ae0b60afebfa9329f64324389c64ff9e
TeddyHuZz <feigaming04XP@gmail.com>
2025-04-09 23:42:43 +0800

    bug fixing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import About from './About';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<About />);
