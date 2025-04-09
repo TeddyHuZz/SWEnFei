a6bcd70f440d1cf14671ce136058f04c285a39a1
TeddyHuZz <feigaming04XP@gmail.com>
2025-04-09 19:44:23 +0800

    Try 3

--- src/About.js
+++ src/About.js
@@ -17,29 +17,31 @@ function About() {
         </nav>
       </div>
       <div className="content">
-        <img src={profilePicture} alt="Me" className="profile-picture" />
-        <div className="text-section">
-          <h1>
-            Hey, I am Wen Fei! Software Developer and Web3{' '}
-            <span className="typing-effect">Enthusiast</span>
-          </h1>
-          <div className="social-media">
-            <a href="https://x.com/@TeddyHuZz" target="_blank" rel="noopener noreferrer">
-              <img src={twitterLogo} alt="Twitter" className="social-logo" />
-              <span>Twitter</span>
-            </a>
-            <a href="https://www.linkedin.com/in/keewenfeicyber" target="_blank" rel="noopener noreferrer">
-              <img src={linkedinLogo} alt="LinkedIn" className="social-logo" />
-              <span>LinkedIn</span>
-            </a>
-            <a href="https://github.com/TeddyHuZz" target="_blank" rel="noopener noreferrer">
-              <img src={githubLogo} alt="GitHub" className="social-logo" />
-              <span>GitHub</span>
-            </a>
-            <a href="https://www.instagram.com/wenfei04/" target="_blank" rel="noopener noreferrer">
-              <img src={InstagramLogo} alt="Instagram" className="social-logo" />
-              <span>Instagram</span>
-            </a>
+        <div className="container">
+          <img src={profilePicture} alt="Me" className="profile-picture" />
+          <div className="text-section">
+            <h1>
+              Hey, I am Wen Fei! Software Developer and Web3{' '}
+              <span className="typing-effect">Enthusiast</span>
+            </h1>
+            <div className="social-media">
+              <a href="https://x.com/@TeddyHuZz" target="_blank" rel="noopener noreferrer">
+                <img src={twitterLogo} alt="Twitter" className="social-logo" />
+                <span>Twitter</span>
+              </a>
+              <a href="https://www.linkedin.com/in/keewenfeicyber" target="_blank" rel="noopener noreferrer">
+                <img src={linkedinLogo} alt="LinkedIn" className="social-logo" />
+                <span>LinkedIn</span>
+              </a>
+              <a href="https://github.com/TeddyHuZz" target="_blank" rel="noopener noreferrer">
+                <img src={githubLogo} alt="GitHub" className="social-logo" />
+                <span>GitHub</span>
+              </a>
+              <a href="https://www.instagram.com/wenfei04/" target="_blank" rel="noopener noreferrer">
+                <img src={InstagramLogo} alt="Instagram" className="social-logo" />
+                <span>Instagram</span>
+              </a>
+            </div>
           </div>
         </div>
         <div className="terminal">
