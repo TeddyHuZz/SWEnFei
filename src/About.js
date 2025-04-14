5f27a9952e72f4936a8b07ff857f111e3670397f
TeddyHuZz <feigaming04XP@gmail.com>
2025-04-14 22:56:42 +0800

    adding page title

--- src/About.js
+++ src/About.js
@@ -7,6 +7,7 @@ import linkedinLogo from './assets/linkedin.png';
 import githubLogo from './assets/github.png';
 import InstagramLogo from './assets/instagram.png';
 
+<title>About Me</title>
 function About() {
   return (
     <div className="container">
