d8fa7973a3c4df9c61141bdc945b788734a164d6
TeddyHuZz <feigaming04XP@gmail.com>
2025-04-15 00:02:22 +0800

    changing github link logo size

--- src/Projects.js
+++ src/Projects.js
@@ -56,9 +56,6 @@ function Projects() {
                   <a href="https://github.com/TeddyHuZz/my-portfolio" className="github-link">
                     <img src="/assets/github-link.png" alt="Github" className="githublink-logo" />
                   </a>
-                  <a href="https://www.linkedin.com/in/keewenfeicyber" className="social-link"> LinkedIn</a>, 
-                  <a href="https://github.com/TeddyHuZz" className="social-link"> GitHub</a>, 
-                  <a href="https://x.com/@TeddyHuZz" className="social-link"> Twitter</a> 
                 ]
               </p>
             </div>
