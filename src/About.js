a90096276ff86010e64c9e812eb81904f05d326a
TeddyHuZz <feigaming04XP@gmail.com>
2025-04-15 13:37:54 +0800

    importing useEffect from react

--- src/About.js
+++ src/About.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './About.css';
 import profilePicture from './assets/profile.jpg';
